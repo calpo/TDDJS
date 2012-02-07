535c022f01021ef7ba8a6f6e5c2e0cb600b11985
skanno <skanno@nttr.co.jp>
2012-02-07 18:11:58 +0900

    11章を写経結果に置き換え

--- 11-the-observer-pattern/src/observable.js
+++ 11-the-observer-pattern/src/observable.js
@@ -1,52 +1,48 @@
-/*jslint indent: 2, plusplus: false, eqeqeq: false, nomen: false, onevar: false*/
-/*globals tddjs*/
 (function () {
-  function _observers(observable, event) {
-    if (!observable.observers) {
-      observable.observers = {};
-    }
+	function _observers(observable, event) {
+		if (!observable.observers) {
+			observable.observers = {};
+		}
 
-    if (!observable.observers[event]) {
-      observable.observers[event] = [];
-    }
+		if (!observable.observers[event]) {
+			observable.observers[event] = [];
+		}
 
-    return observable.observers[event];
-  }
+		return observable.observers[event];
+	}
 
-  function observe(event, observer) {
-    if (typeof observer != "function") {
-      throw new TypeError("observer is not function");
-    }
+	function observe(event, observer) {
+		if (typeof observer != "function") {
+			throw new TypeError("observer is not function");
+		}
+		_observers(this, event).push(observer);
+	}
 
-    _observers(this, event).push(observer);
-  }
+	function hasObserver(event, observer) {
+		var observers = _observers(this, event);
+		for (var i = 0, l = observers.length; i < l; i++) {
+			if (observers[i] == observer) {
+				return true;
+			}
+		}
 
-  function hasObserver(event, observer) {
-    var observers = _observers(this, event);
+		return false;
+	}
 
-    for (var i = 0, l = observers.length; i < l; i++) {
-      if (observers[i] == observer) {
-        return true;
-      }
-    }
+	function notify(event) {
+		var observers = _observers(this, event);
+		var args = Array.prototype.slice.call(arguments, 1);
 
-    return false;
-  }
+		for (var i = 0, l = observers.length; i < l; i++) {
+			try{
+				observers[i].apply(this, args);
+			}catch (e) {}
+		}
+	}
 
-  function notify(event) {
-    var observers = _observers(this, event);
-    var args = Array.prototype.slice.call(arguments, 1);
-
-    for (var i = 0, l = observers.length; i < l; i++) {
-      try {
-        observers[i].apply(this, args);
-      } catch (e) {}
-    }
-  }
-
-  tddjs.namespace("util").observable = {
-    observe: observe,
-    hasObserver: hasObserver,
-    notify: notify
-  };
+	tddjs.namespace("util").observable = {
+		observe: observe,
+		hasObserver: hasObserver,
+		notify: notify
+	};
 }());
--- 11-the-observer-pattern/test/observable_test.js
+++ 11-the-observer-pattern/test/observable_test.js
@@ -1,132 +1,138 @@
 /*jslint indent: 2, onevar: false*/
-/*globals TestCase, assertEquals, assertTrue, assertFalse,assertException, assertNoException, tddjs*/
-TestCase("ObservableAddObserverTest", {
-  setUp: function () {
-    this.observable = Object.create(tddjs.util.observable);
-  },
+/*globals TestCase, assertEquals, tddjs*/
+TestCase("ObservableAddObserver", {
+	setUp: function () {
+		this.observable = Object.create(tddjs.util.observable);
+	},
 
-  "test should store functions": function () {
-    var observers = [function () {}, function () {}];
+	"test should store functions": function () {
+		var observers = [function () {}, function () {}];
 
-    this.observable.observe("event", observers[0]);
-    this.observable.observe("event", observers[1]);
+		this.observable.observe("event", observers[0]);
+		this.observable.observe("event", observers[1]);
 
-    assertTrue(this.observable.hasObserver("event", observers[0]));
-    assertTrue(this.observable.hasObserver("event", observers[1]));
-  },
-
-  "test should throw for uncallable observer": function () {
-    var observable = this.observable;
-
-    assertException(function () {
-      observable.observe("event", {});
-    }, "TypeError");
-  }
+		assertTrue(this.observable.hasObserver("event", observers[0]));
+		assertTrue(this.observable.hasObserver("event", observers[1]));
+	}
 });
 
 TestCase("ObservableHasObserverTest", {
-  setUp: function () {
-    this.observable = Object.create(tddjs.util.observable);
-  },
+	setUp: function () {
+		this.observable = Object.create(tddjs.util.observable);
+	},
 
-  "test should return false when no observers": function () {
-    assertFalse(this.observable.hasObserver(function () {}));
-  }
+	"test should return false when don't has observer": function () {
+		var observer = function () {};
+		var dummy = function () {};
+
+		this.observable.observe("event", observer);
+
+		assertFalse(this.observable.hasObserver("event", dummy));
+	}
 });
 
 TestCase("ObservableNotifyObserversTest", {
-  setUp: function () {
-    this.observable = Object.create(tddjs.util.observable);
-  },
+	setUp: function () {
+		this.observable = Object.create(tddjs.util.observable);
+	},
 
-  "test should call all observers": function () {
-    var observer1 = function () {
-      observer1.called = true;
-    };
+	"test should call all observers": function () {
+		var observer1 = function () {
+			observer1.called = true;
+		};
+		var observer2 = function () {
+			observer2.called = true;
+		};
 
-    var observer2 = function () {
-      observer2.called = true;
-    };
+		this.observable.observe("event", observer1);
+		this.observable.observe("event", observer2);
 
-    this.observable.observe("event", observer1);
-    this.observable.observe("event", observer2);
+		this.observable.notify("event");
 
-    this.observable.notify("event");
+		assertTrue(observer1.called);
+		assertTrue(observer2.called);
+	},
 
-    assertTrue(observer1.called);
-    assertTrue(observer2.called);
-  },
+	"test should pass through arguments": function () {
+		var actual1, actual2, actuals;
 
-  "test should pass through arguments": function () {
-    var actual;
+		var observer1 = function (p1, p2) {
+			actual1 = p1;
+			actual2 = p2;
+		};
+		var observer2 = function () {
+			actuals = arguments;
+		};
 
-    this.observable.observe("event", function () {
-      actual = arguments;
-    });
+		this.observable.observe("event", observer1);
+		this.observable.observe("event", observer2);
+		this.observable.notify("event", "hoge", 2);
 
-    this.observable.notify("event", "String", 1, 32);
+		assertEquals("hoge", actual1);
+		assertEquals(2, actual2);
+		assertEquals(["hoge", 2], actuals);
+	},
 
-    assertEquals(["String", 1, 32], actual);
-  },
+	"test should throw fro uncallable observer": function () {
+		assertException(function () {
+			this.observable.observe("event", {});
+		}, "TypeError");
+	},
 
-  "test should notify all even when some fail": function () {
-    var observer1 = function () {
-      throw new Error("Oops");
-    };
+	"test should notify all even when some fail": function () {
+		var observer1 = function () {
+			throw new Error("Oops");
+		};
+		var observer2 = function () {
+			observer2.called = true;
+		};
 
-    var observer2 = function () {
-      observer2.called = true;
-    };
+		this.observable.observe("event", observer1);
+		this.observable.observe("event", observer2);
 
-    this.observable.observe("event", observer1);
-    this.observable.observe("event", observer2);
+		this.observable.notify("event");
 
-    this.observable.notify("event");
+		assertTrue(observer2.called);
+	},
 
-    assertTrue(observer2.called);
-  },
+	"test should call observers in the order they were added": function () {
+		var calls = [];
+		var observer1 = function () {
+			calls.push(observer1);
+		};
+		var observer2 = function () {
+			calls.push(observer2);
+		};
 
-  "test should call observers in the order they were added": function () {
-    var calls = [];
+		this.observable.observe("event", observer1);
+		this.observable.observe("event", observer2);
 
-    var observer1 = function () {
-      calls.push(observer1);
-    };
+		this.observable.notify("event");
 
-    var observer2 = function () {
-      calls.push(observer2);
-    };
+		assertEquals(observer1, calls[0]);
+		assertEquals(observer2, calls[1]);
+	},
 
-    this.observable.observe("event", observer1);
-    this.observable.observe("event", observer2);
+	"test should not fail if no observers": function () {
+		var observable = this.observable;
+		assertNoException(function () {
+			observable.notify("event");
+		});
+	},
 
-    this.observable.notify("event");
+	"test should notify relevant observers only": function () {
+		var calls = [];
 
-    assertEquals(observer1, calls[0]);
-    assertEquals(observer2, calls[1]);
-  },
+		this.observable.observe("event", function () {
+			calls.push("event");
+		});
 
-  "test should not fail if no observers": function () {
-    var observable = this.observable;
+		this.observable.observe("other", function () {
+			calls.push("other");
+		});
 
-    assertNoException(function () {
-      observable.notify("event");
-    });
-  },
+		this.observable.notify("other");
 
-  "test should notify relevant observers only": function () {
-    var calls = [];
-
-    this.observable.observe("event", function () {
-      calls.push("event");
-    });
-
-    this.observable.observe("other", function () {
-      calls.push("other");
-    });
-
-    this.observable.notify("other");
-
-    assertEquals(["other"], calls);
-  }
+		assertEquals(["other"], calls);
+	}
 });

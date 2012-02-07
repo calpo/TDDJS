0efd5416d7a856fbd013f697fb7907506abc925e
skanno <skanno@nttr.co.jp>
2012-02-07 19:21:11 +0900

    手動stubの前 git log --oneline 03474d6.. git diff 1408f6b c4d9b85

---
+++ 12-abstracting-browser-differences-ajax/test/request_test.js
@@ -0,0 +1,18 @@
+(function () {
+	var ajax = tddjs.ajax;
+
+	TestCase("GetRequestTest", {
+		"test should define get method": function () {
+			assertFunction(ajax.get);
+		},
+
+		"test should throw error without url": function () {
+			assertException(function () {
+				ajax.get();
+			}, "TypeError");
+		},
+
+		"": function () {
+		}
+	});
+}());

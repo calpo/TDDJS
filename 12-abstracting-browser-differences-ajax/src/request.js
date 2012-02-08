9d95731ef924a1ab157eee0a9e3ffae658f0c060
skanno <skanno@nttr.co.jp>
2012-02-08 19:34:10 +0900

    git diff 15eb688 d18734c まで終了 git log --oneline 15eb688.. transport is undefined でエラーが残ったまま

--- 12-abstracting-browser-differences-ajax/src/request.js
+++ 12-abstracting-browser-differences-ajax/src/request.js
@@ -5,6 +5,9 @@
 		if (typeof url != "string") {
 			throw new TypeError("URL should be string");
 		}
+
+		var transport = tddjs.ajax.create();
+		transport.open("GET", url, true);
 	}
 
 	ajax.get = get;
--- 12-abstracting-browser-differences-ajax/test/request_test.js
+++ 12-abstracting-browser-differences-ajax/test/request_test.js
@@ -2,6 +2,14 @@
 	var ajax = tddjs.ajax;
 
 	TestCase("GetRequestTest", {
+		setUp: function () {
+			this.originalCreate = ajax.create;
+		},
+
+		setDown: function () {
+			this.create = ajax.originalCreate;
+		},
+
 		"test should define get method": function () {
 			assertFunction(ajax.get);
 		},
@@ -12,7 +20,22 @@
 			}, "TypeError");
 		},
 
-		"": function () {
+		"test should obtain an XMLHttpRequest object": function () {
+			ajax.create = stubFn();
+			ajax.get("/url");
+
+			assert(ajax.create.called);
+		},
+
+		"test should call open() with valid params": function () {
+			var openStub = stubFn();
+			ajax.create = stubFn({
+				open: openStub
+			});
+			var url = "/url";
+			ajax.get(url);
+
+			assertEquals(["GET", url, true], openStub.args);
 		}
 	});
 }());
